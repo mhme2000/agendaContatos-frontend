51358d481e79001fe97e034b5ce38eb6dcd96451
Marcos Henrique Mainardi Eckart <marcos.eckart@loglabdigital.com.br>
2021-12-17 21:07:12 -0400

    Config tests and material ui

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('render text in screen', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Agenda de Contatos/i);
   expect(linkElement).toBeInTheDocument();
 });
